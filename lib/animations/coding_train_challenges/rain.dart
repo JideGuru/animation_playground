2597bd78118bf0b0745231ac5b15441e0fef9c65
jideguru <guruliciousjide@gmail.com>
2023-03-04 20:20:40 +0100

    fix rain animation

--- lib/animations/coding_train_challenges/rain.dart
+++ lib/animations/coding_train_challenges/rain.dart
@@ -2,6 +2,7 @@ import 'dart:math';
 
 import 'package:animation_playground/utils.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/scheduler.dart';
 
 class RainAnimation extends StatelessWidget {
   const RainAnimation({Key? key}) : super(key: key);
@@ -13,7 +14,7 @@ class RainAnimation extends StatelessWidget {
       backgroundColor: Colors.black,
       body: Stack(
         children: [
-          for (int index = 0; index < 500; index++)
+          for (int index = 0; index < 200; index++)
             _RainDrop(
               screenHeight: screenSize.height,
               screenWidth: screenSize.width,
@@ -37,75 +38,57 @@ class _RainDrop extends StatefulWidget {
   State<_RainDrop> createState() => _RainDropState();
 }
 
-class _RainDropState extends State<_RainDrop> with TickerProviderStateMixin {
-  late AnimationController animationController;
-  late Animation animation;
-  late double dx, dy, length, z;
+class _RainDropState extends State<_RainDrop> with SingleTickerProviderStateMixin {
+  // vy(velocity Y) for the speed on the yAxis
+  late double dx, dy, length, z, vy;
 
   Random random = Random();
-
   double get screenHeight => widget.screenHeight;
 
   double get screenWidth => widget.screenWidth;
+  late final Ticker _ticker;
 
   @override
   void initState() {
     super.initState();
+    randomizeValues();
+    _ticker = createTicker((elapsed) {
+      dy += vy;
+      if (dy >= screenHeight + 100) {
+        randomizeValues();
+      }
+      setState(() {});
+    });
+    _ticker.start();
+  }
+
+  randomizeValues() {
     dx = random.nextDouble() * screenWidth;
     dy = -500 - (random.nextDouble() * -500);
     z = random.nextDouble() * 20;
     length = rangeMap(z, 0, 20, 10, 20);
-    animationController = AnimationController(
-      vsync: this,
-      duration: Duration(milliseconds: rangeMap(z, 0, 20, 3000, 500).toInt()),
-    );
-    animation = Tween<double>(begin: dy, end: screenHeight).animate(
-      animationController,
-    );
-    animationController.forward();
-
-    animationController.addListener(animationListener);
-  }
-
-  animationListener() {
-    if (animationController.status == AnimationStatus.completed) {
-      animationController.reset();
-      z = random.nextDouble() * 20;
-      length = rangeMap(z, 0, 20, 10, 20);
-      animationController.duration = Duration(
-        milliseconds: rangeMap(z, 0, 20, 3000, 500).toInt(),
-      );
-      dx = random.nextDouble() * screenWidth;
-      dy = -500 - (random.nextDouble() * -500);
-      if (mounted) animationController.forward();
-    }
+    vy = rangeMap(z, 0, 20, 15, 5);
   }
 
   @override
   void dispose() {
-    animationController.removeListener(animationListener);
-    animationController.dispose();
+    _ticker.dispose();
     super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
-    return AnimatedBuilder(
-      animation: animation,
-      builder: (context, child) {
-        return Transform.translate(
-          offset: Offset(dx, animation.value),
-          child: Container(
-            height: length,
-            width: 2,
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(5),
-              color: Colors.white,
-              border: Border.all(width: rangeMap(z, 0, 20, 1, 3)),
-            ),
-          ),
-        );
-      },
+    return Transform.translate(
+      offset: Offset(dx, dy),
+      child: Container(
+        height: length,
+        width: 2,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(5),
+          color: Colors.white,
+          border: Border.all(width: rangeMap(z, 0, 20, 1, 3)),
+        ),
+      ),
     );
   }
 }
